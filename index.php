f9129fda55d24439f9478c0a5285323934e6a40a
akaam <akaamil@outlook.com>
2018-09-24 10:20:27 +0300

    First Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+
+<head> <title> php Boot camp  </title></head>
+<body>
+PHP first pracital course class
+<br>
+</body>
+</html>
+
+
+<?php
+echo "this is echo contect  ";
+
+print "this is print content ";
+
+?>
