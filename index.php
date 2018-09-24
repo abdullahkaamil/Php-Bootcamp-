62136be5e8b658d22cb15646dc32c428967dbf0d
akaam <akaamil@outlook.com>
2018-09-24 11:19:51 +0300

    conditional Statement

--- index.php
+++ index.php
@@ -8,25 +8,11 @@ PHP first pracital course class
 </html>
 
 <?php
-//Arithematical Operators
-$int1 =8;
-$int2 =7; 
-$ans1 = $int1 + $int2;
-echo $ans1;
-echo "<br>";
-
-$ans2 = $int1 - $int2;
-echo $ans2;
-echo "<br>";
-
-$ans3 =$int1 /  $int2;
-echo $ans3;
-echo "<br>";
-
-$ans4 = $int1 % $int2;
-echo $ans4;
-echo "<br>";
- 
-echo ++$ans4;
+//conditional statements 
+if (5 < 7) {
+    echo "its working ";
+} else {
+    echo "not working";
+}
 
 ?>
