dfb8781a18a14895a021f716295ff53d6510f4dd
akaam <akaamil@outlook.com>
2018-09-24 10:38:24 +0300

    using PHP inside HTML...

--- index.php
+++ index.php
@@ -4,13 +4,16 @@
 <body>
 PHP first pracital course class
 <br>
+<?php
+echo"<p>php inside html</p>";
+
+?>
 </body>
 </html>
 
-
 <?php
 echo "this is echo contect  ";
-
+echo "<h2>using html inside html</h2>";
 print "this is print content ";
 
 ?>
