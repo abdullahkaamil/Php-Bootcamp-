cd5f1466a92d1806ba1b6af92bdf45cc5aedeae4
akaam <akaamil@outlook.com>
2018-09-24 11:45:48 +0300

    switch case2

--- index.php
+++ index.php
@@ -9,20 +9,20 @@ PHP first pracital course class
 
 <?php
 //switch case 
-$var1 = "jeo";
+$var1 = "mark";
 switch ($var1) {
     case "jeo":
-        echo " the value of the var1 is jeo";
+        $result =" the value of the var1 is jeo";
         break;
     case "mark":
-        echo "the value is mark";
+    $result = "the value is mark";
         break;
     case "shahzaib":
-        echo "the value is abdullah";
+    $result = "the value is abdullah";
         break;
     default:
-        echo "this is the defulat value";
+    $result = "this is the defulat value";
         break;
 }
-
+print $result;
 ?>
